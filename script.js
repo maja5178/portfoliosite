2695d2e7d1da71e31b28c28fb3618294be7c15e0
Maja kepler <maja@maja2.local>
2020-01-01 19:49:48 +0100

    burgermenu media virker

--- script.js
+++ script.js
@@ -1,47 +1,13 @@
-let sliderImages = document.querySelectorAll(".slide"),
-    arrowLeft = document.querySelector("#arrow_left"),
-    arrowRight = document.querySelector("#arrow_right"),
-    current = 0;
+window.addEventListener("load", navShow);
 
-function reset() {
-    for (let i = 0; i < sliderImages.length; i++) {
-        sliderImages[i].style.display = "none";
-    }
-}
-// init slider
-function startSlide() {
-    reset();
-    sliderImages[0].style.display = "block";
 
-}
-// show prev
-function slideLeft() {
-    reset();
-    sliderImages[current - 1].style.display = "block";
-    current--;
+function navShow() {
+    console.log("navShow");
+
+    document.querySelector(".burger").addEventListener("click", burgerClick);
 }
 
-//show next
-function slideRight() {
-    reset();
-    sliderImages[current + 1].style.display = "block";
-    current++;
+function burgerClick() {
+    console.log("burgerClick");
+    document.querySelector('.nav_links').classList.toggle("hide");
 }
-
-//left arrow click
-arrowLeft.addEventListener("click", function () {
-    if (current === 0) {
-        current = sliderImages.length;
-    }
-    slideLeft();
-});
-
-//right arrow click
-arrowRight.addEventListener("click", function () {
-    if (current === sliderImages.length - 1) {
-        current = -1;
-    }
-    slideRight();
-});
-
-startSlide();
